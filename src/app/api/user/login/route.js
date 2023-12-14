1531870c6c5405c1c05818aef7bdb3ab0bd844a9
Ganesh Dwivedi <90120686+ganeshdwivedi@users.noreply.github.com>
2023-12-15 00:05:55 +0530

    Update route.js

--- src/app/api/user/login/route.js
+++ src/app/api/user/login/route.js
@@ -1,7 +1,7 @@
 import connect from "../../../../config/dbconfig";
 import User from "../../../../model/userModel";
 import argon2 from "argon2";
-import { generateToken } from "../../../../config/generateToken";
+import { generateToken } from "@/config/generateToken";
 import { NextResponse } from "next/server";
 
 export async function POST(request) {
